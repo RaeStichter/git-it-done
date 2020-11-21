ba1094587c730f42b423b86adc8c6ba3ba0119db
Rae Stichter <raestichter@gmail.com>
2020-11-21 18:14:24 -0500

    search repos with user names and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,11 @@
+// Variables added to store reference from the form element with an id of user form and the input element with anid of user name
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// variables for the right side repo dispolay
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 // this was the original getUser repo when octocar was hard coded.
 
 // var getUserRepos = function() {
@@ -22,15 +30,111 @@
 // };
 
 // ------------------------------- Dynamic Call Repo -------------------------------
+// see code above for the original
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+    // make a request to the url.  Uodated this to include error handling.
+    fetch(apiUrl)
+        .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) { //catched network errors
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
     });
   };  
-getUserRepos(); // can now try to call using getUserRepos("microsoft") or getUserRepos("facebook").
+//getUserRepos(); // can now try to call using getUserRepos("microsoft") or getUserRepos("facebook").
+// commented out because now we are using an event listener to look for a click
+
+// ------------------------------- Submit handler from form -------------------------------
+// gets information from the click event.  This was the original code:
+            // var formSubmitHandler = function(event) {
+            //     event.preventDefault(); // prevents default action of browser, we then can specify what to do
+            //     console.log(event); //
+            // };
+
+var formSubmitHandler = function(event) {
+    event.preventDefault(); // prevents default action of browser, we then can specify what to do
+    // get value from input element
+    var username = nameInputEl.value.trim(); // get the input from the form.  trim() to get rd of any extra spaces at beg or end
+
+    if (username) {         // if there is a username  entered
+        getUserRepos(username); // call function with the entered user name
+        nameInputEl.value = ""; // then clear the form to get ready for the next request
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+// ------------------------------- Display Repos -------------------------------
+// this function qill acccept both array of repo data and the term we searched for as paramenters.
+// inputs to this function come from the getUserRepo function
+
+var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+    
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm; // this will display the search term on the screen
+
+    console.log(repos);
+    console.log(searchTerm);
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+
+        // -------------------- this block diaplys the repos names
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+    
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+    
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+    
+        // append to container
+        repoEl.appendChild(titleEl);
+        // -------------------- end repos names block
+
+        //  -------------- this block pulls the issues for the repo
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // ------------- end status block
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+  };
+
+
+
+  userFormEl.addEventListener("submit", formSubmitHandler);
